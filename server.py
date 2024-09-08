51809ad8e4ce34b81e802486e8b28ce16172a9b8
Maksimov86 <tylerbk666@gmail.com>
2024-09-08 17:19:35 -0400

    final version

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+"""
+This is a docstring for my_module
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,21 +8,26 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def emo_detector():
+    """
+    This is a docstring for def emo_detector()
+    """
     text_to_analyze = request.args.get("textToAnalyze")
 
     dominant_emotion = emotion_detector(text_to_analyze)
 
     if dominant_emotion is None:
         return "Invalid text! Please try again!"
-    else:
-        return f"For the given statement, the system response is 'anger': \
-        {dominant_emotion['anger']}, 'disgust': {dominant_emotion['disgust']}, \
-        'fear': {dominant_emotion['fear']}, 'joy': {dominant_emotion['joy']} and \
-        'sadness': {dominant_emotion['sadness']}. The dominant emotion is \
-        {dominant_emotion['dominant_emotion']}"
+    return f"For the given statement, the system response is 'anger': \
+    {dominant_emotion['anger']}, 'disgust': {dominant_emotion['disgust']}, \
+    'fear': {dominant_emotion['fear']}, 'joy': {dominant_emotion['joy']} and \
+    'sadness': {dominant_emotion['sadness']}. The dominant emotion is \
+    {dominant_emotion['dominant_emotion']}"
 
 @app.route("/")
 def render_index_page():
+    """
+    This is a docstring for def render_index_page()
+    """
     return render_template("index.html")
 
 if __name__ == "__main__":
