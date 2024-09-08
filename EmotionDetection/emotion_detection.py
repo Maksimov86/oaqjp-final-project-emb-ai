cc15fc88df858b39ffa3ad04166b168617f54825
Maksimov86 <tylerbk666@gmail.com>
2024-09-08 18:17:47 -0400

    final version

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,28 +1,28 @@
 import requests
 import json
 
-def emotion_detector(text_to_analyze):
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    myobj = { "raw_document": { "text": text_to_analyze } }
-    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    response = requests.post(url, json = myobj, headers=header)
-    res = json.loads(response.text)
-    formatted_response = {}
+def emotion_detector(text_to_analyse):
 
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = {"raw_document": { "text": text_to_analyse } }
+    response = requests.post(url, json = myobj, headers = headers)
+    status_code = response.status_code
+    
     if response.status_code == 200:
+        res = json.loads(response.text)
+        formatted_response = {}
         formatted_output = res['emotionPredictions'][0]['emotion']
         dominant_emotion = max(formatted_output, key=lambda x: formatted_output[x])
         formatted_response['dominant_emotion'] = dominant_emotion
         formatted_output.update(formatted_response)
         return formatted_output
-    #elif response.status_code == 400:
-        #return {
-               #'anger': None,
-               #'disgust': None,
-               #'fear': None,
-               #'joy': None,
-               #'sadness': None,
-               #'dominant_emotion': None,
-               #}
-    else:
-        None
+    elif status_code == 400:
+        return { 
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None 
+            }
--- server.py
+++ server.py
@@ -1,34 +1,31 @@
-"""
-This is a docstring for my_module
-"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
-def emo_detector():
-    """
-    This is a docstring for def emo_detector()
-    """
-    text_to_analyze = request.args.get("textToAnalyze")
+def emotion_detector_function():
+    ''' This function calls the application
+    '''
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyze)
 
-    dominant_emotion = emotion_detector(text_to_analyze)
+    if response['dominant_emotion'] is None:
+        response_text = "Invalid Input! Please try again."
+    else:
+        response_text = f"For the given statement, the system response is 'anger': \
+                    {response['anger']}, 'disgust': {response['disgust']}, \
+                    'fear': {response['fear']}, 'joy': {response['joy']}, \
+                    'sadness': {response['sadness']}. The dominant emotion is \
+                    {response['dominant_emotion']}."
 
-    if dominant_emotion is None:
-        return "Invalid text! Please try again!"
-    return f"For the given statement, the system response is 'anger': \
-    {dominant_emotion['anger']}, 'disgust': {dominant_emotion['disgust']}, \
-    'fear': {dominant_emotion['fear']}, 'joy': {dominant_emotion['joy']} and \
-    'sadness': {dominant_emotion['sadness']}. The dominant emotion is \
-    {dominant_emotion['dominant_emotion']}"
+    return response_text
 
 @app.route("/")
 def render_index_page():
-    """
-    This is a docstring for def render_index_page()
-    """
-    return render_template("index.html")
+    ''' This is the function to render the html interface
+    '''
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host="localhost", port=5000, debug=True)
+    app.run(host = "0.0.0.0", port = 5000)
